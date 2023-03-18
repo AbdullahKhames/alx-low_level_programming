9af8bb2766243c4aa63a3014888e992ac4e9190e
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-03-17 23:58:15 -0700

    fixed betty style erroe

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,8 +6,11 @@
 /* betty style doc for function main goes there */
 /**
  * main - main function
- *
- * Return : always 0 
+ *Description :enter a random number
+ * and check its value is over 0 print positive
+ * if less than zero prints negative
+ * if zero prints zero
+ * Return:0
  */
 
 int main(void)
@@ -19,22 +22,17 @@ int main(void)
 	char zero[] = "is zero";
 
 	char negative[] = "is negative";
+
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	/* your code goes there */
+
 	if (n > 0)
-       	{
-	
 		printf("%d %s\n", n, positive);
-	}
 	else if (n == 0)
-       	{
-	
 		printf("%d %s\n", n, zero);
-	}
-	else 
-	{	
 		printf("%d %s\n", n, negative);
-	}
 	return (0);
 }
