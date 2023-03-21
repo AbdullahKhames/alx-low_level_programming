a46128966ffdc991587186c671044a8ee56d98ce
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-03-21 22:33:04 +0200

    second task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - function
+ *
+ *Description : 'new function to print alphabet'
+ *
+ *Return : returns the alphabet
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+	char ch;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
