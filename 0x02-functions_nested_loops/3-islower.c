179b7993d2af8a7153e2b069ee50e3479510c7d8
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-03-21 22:46:59 +0200

    3rd task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - function to check if character is loser case
+ *
+ *@c : the parameter to be checked is loswer
+ *
+ *Description : 'new function to check alphabet'
+ *
+ *Return : returns 0 if lower case returns  1 if capital case
+ */
+
+int _islower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else if (c >= 'a' && c <= 'z')
+		return (0);
+}
