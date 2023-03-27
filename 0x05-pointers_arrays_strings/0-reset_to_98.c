03e666673c1b754e9e1285823df93e02e09cd835
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-03-27 13:39:46 -0700

    revert to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that swaps integers
+ * @*n: first argument to be swapped passed by reference
+ * Description: function to swap int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
