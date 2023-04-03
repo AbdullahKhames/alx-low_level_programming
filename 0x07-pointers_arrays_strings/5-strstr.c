341fa1718357436f0502b1b68e43ee8c9b987784
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-03 17:28:44 +0000

    new

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strstr
+ * _strstr - finds first occurence of needle in haystack
  * @haystack: first arg
  * @needle: search string
- * Return: returns occurecnce of second in first 
+ * Return: returns occurecnce of second in first
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -15,7 +15,23 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (index2 = 0; needle[index2] != '\0'; index2++)
 		{
-			
+			if (haystack[index1] == needle[0])
+			{
+				if (haystack[index2] == needle[index2])
+				{
+					index2++;
+				}
+				else
+					break;
+			}
 		}
+		if (needle[index2])
+		{
+			index1++;
+			j = 0;
+		}
+		else
+			return (haystack + index1);
 	}
+	return (0);
 }
