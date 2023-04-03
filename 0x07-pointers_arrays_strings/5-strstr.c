92f729b8fc8a0bae7ce754f055638eb5da9fb86d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-03 16:13:50 +0000

    new

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strstr
+ * @haystack: first arg
+ * @needle: search string
+ * Return: returns occurecnce of second in first 
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index1, index2;
+	char *cptr;
+
+	for (index1 = 0; haystack[index1] != '\0'; index1++)
+	{
+		for (index2 = 0; needle[index2] != '\0'; index2++)
+		{
+			
+		}
+	}
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints diagsumss
+ *
+ * @a: first arg
+ *
+ * @size: second arg
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int index1;
+	int x = 0, y = 0;
+
+	for (index1 = 0; index1 < size; index1++)
+	{
+		x += a[index1];
+		y += a[size - index1 -1];
+		a += size;
+	}
+	printf("%d, ", x);
+	printf("%d\n", y);
+}
