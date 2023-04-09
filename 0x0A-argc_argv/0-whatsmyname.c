7290e6877daf89cd304b86c6a136f80950a5644d
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-09 12:34:00 +0000

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prog
+ * @argc: arg
+ * @argv: arg
+ * Return: returns
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc-1]);
+	return (0);
 }
