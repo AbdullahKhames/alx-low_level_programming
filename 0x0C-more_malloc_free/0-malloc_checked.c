c541a75c4d448988aee77d0a8bf078222d8ff27c
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-14 21:08:46 +0000

    new

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function to creates memory allocateion
+ * @b: int of alloc
+ * Return: return the address of allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *iptr = malloc(b);
+
+	if (iptr ==)
+		exit(98);
+	return (iptr);
+}
