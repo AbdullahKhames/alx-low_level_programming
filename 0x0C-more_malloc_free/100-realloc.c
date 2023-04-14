2a9bb58d257c224a2e70719ce5ac1cb06aa5a461
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-14 21:45:49 +0000

    new

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		if (p == NULL)
 			return (NULL);
-		return (P);
+		return (p);
 	}
 	if (new_size > old_size)
 	{
