5774f5abb5ee6c786be1b79edffc9302a42a11fd
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-04-27 23:24:35 +0000

    new

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - function to calculate how many items in list
+ * @h: the list given
+ * Return: return number of items on lisr
+ */
+
+size_t list_len(const list_t *h)
+{
+	const list_t temp = h;
+	size_t i = 0;
+
+	if (!h)
+		return (i);
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		i++
+	}
+	return (i);
+}
