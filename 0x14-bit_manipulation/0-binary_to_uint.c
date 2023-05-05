251b1dce05a220ed3de58456c9b0ae1b4a1089d2
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-05-05 02:53:15 +0000

    initital

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int number;
+
+	if (!b)
+		return (0);
+
+	while (*b)
+	{
+		if (*b != '0' || *b != '1')
+			return (0);
+		number = number * 2 + (*b++ - '0');
+	}
+	return (number);
+}
