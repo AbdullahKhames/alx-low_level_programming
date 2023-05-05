e37e3ebb013dec2613e5543a4921cb8caa0e81c7
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-05-05 17:33:48 +0000

    fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,54 @@
 #include "main.h"
 
+/***/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number;
+	unsigned int number = 0;
+	int i = 0;
 
 	if (!b)
 		return (0);
 
 	while (*b)
 	{
-		if (*b != '0' || *b != '1')
-			return (0);
-		number = number * 2 + (*b++ - '0');
+		if (*b == '0')
+			number += 0 * pow(2, i);
+		else if (*b == '1')
+			number += 1 * pow(2, i);
+		i++;
 	}
 	return (number);
+}
+
+/***/
+unsigned int pow(unsigned int num, int x)
+{
+
+	int i = 1;
+	unsigned int temp = num;
+
+	if (x == 0)
+		return (1);
+	if (x == 1)
+		return (num);
+
+	for (; i < x; i++)
+		temp = temp * num;
+
+	return (temp);
+}
+
+int _strlen(char *str)
+{
+    int length = 0;
+
+    if (!(*str))
+        return (0);
+
+    while (*str != '\0')
+    {
+        length++;
+        str++;
+    }
+    return (length);
 }
