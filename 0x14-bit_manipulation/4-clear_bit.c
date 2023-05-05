a5e7bb639d0462816a62d96fef3b24103b7f48bf
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-05-05 21:56:17 +0000

    new

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,14 +4,14 @@
  * clear_bit - function to clear a given index
  * @n: the number
  * @index: the index to clear
- * Return success
+ * Return: success
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int x;
 
-	if (index > (sizeof(unsigned long int) * 8 -1))
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	x = ~(1 << index);
 	*n = *n & x;
