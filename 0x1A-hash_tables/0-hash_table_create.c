bbe67219c39041d9add1141b627af21a919c43fd
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-06 19:56:03 +0300

    allocating space for array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,17 +7,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable;
+	unsigned long int idx;
 
 	if (size <= 0)
 	{
 		return (NULL);
 	}
 
-	hashTable = malloc(sizeof(hash_table_t) * size);
+	hashTable = malloc(sizeof(hash_table_t));
 	if (!hashTable)
 	{
 		perror("error allocating space for hash table");
 		return (NULL);
+	}
+	hashTable->size = size;
+	hashTable->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hashTable->array)
+	{
+		perror("error allocating array");
+		return (NULL);
+	}
+	for (idx = 0; idx < size; idx++)
+	{
+		hashTable->array[idx] = NULL;
 	}
 	return (hashTable);
 }
