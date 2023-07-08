02a9ea2fec2e252b0c200e90b6693187b04a6511
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-07-08 14:00:05 +0300

    printing the table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -32,12 +32,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 				value = node->value;
 				return (value);
 			}
-			
+
 			node = node->next;
 		}
-		
-		
+
 	}
-	
+
 	return (value);
 }
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function to print hash table
+ * @ht: hash table to be printed
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	size_t i = 0;
+	int printed = 0;
+	hash_node_t *node;
+
+	if (!ht)
+	{
+		return;
+	}
+	printf("{");
+	for (; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (printed)
+			{
+				printf(",");
+			}
+			printf("\'%s\': \'%s\'", node->key, node->value);
+			printed = 1;	
+			node = node->next;
+		}
+	}
+	printf("}\n");
+
+	
+}
