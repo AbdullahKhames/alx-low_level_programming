936ec718d700698eba1522d4c03a785a58b450f3
AbdullahKhames <abdallahyouniiss@gmail.com>
2023-08-14 15:04:30 +0300

    new

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,42 @@
 #include "lists.h"
 /**
+ * validate_dnodeint_at_index - function too insert at a given index
+ * @h: list to insert into
+ * @idx: index to insert at
+ * @n: the number to be added
+ * Return: the new node
+ */
+dlistint_t *validate_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current;
+	unsigned int count = 0;
+
+	if (!h)
+	{
+		return (NULL);
+	}
+	if (!(*h))
+	{
+		return (create_first_node(h, n));
+	}
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	current = *h;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	if (idx == count)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	return (NULL);
+}
+/**
  * insert_dnodeint_at_index - function too insert at a given index
  * @h: list to insert into
  * @idx: index to insert at
@@ -11,37 +48,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *before;
 	dlistint_t *current;
 	dlistint_t *new;
-
+	dlistint_t *temp;
 	unsigned int i = 0;
-	unsigned int count = 0;
-	if (!h)
-	{
-		return (NULL);
-	}
-	if (!(*h))
-	{
-		return create_first_node(h, n);
-	}
 
-	if (idx == 0)
-	{
-		return add_dnodeint(h, n);
-	}
-	current = *h;
-	while (current)
-	{
-		count++;
-		current = current->next;
-	}
-	if (idx == count)
-	{
-		return add_dnodeint_end(h, n);
-	}
-	if (idx > count)
-	{
+	if (!h)
 		return (NULL);
+	temp = validate_insert_dnodeint_at_index(h, idx, n);
+	if (temp)
+	{
+		return (temp);
 	}
-	
 	new = malloc(sizeof(dlistint_t));
 	current = *h;
 	new->n = n;
@@ -56,7 +72,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				before->next = new;
 				new->prev = before;
 			}
-			else{
+			else
+			{
 				new->prev = NULL;
 			}
 			return (new);
@@ -67,5 +84,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	free(new);
 	return (NULL);
-
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 /**
+ * new_func - function too insert at a given index
+ * @current: list to insert into
+ * @previous: index to insert at
+ * Return: int
+ */
+int new_func(dlistint_t *current, dlistint_t *previous)
+{
+	if (current->next)
+	{
+		previous->next = current->next;
+		current->next->prev = previous;
+	}
+	else
+	{
+		previous->next = NULL;
+	}
+	free(current);
+	return (1);
+}
+/**
  * delete_dnodeint_at_index - function too insert at a given index
  * @h: list to insert into
  * @idx: index to insert at
@@ -9,7 +29,6 @@ int delete_dnodeint_at_index(dlistint_t **h, unsigned int idx)
 {
 	dlistint_t *current;
 	dlistint_t *previous;
-
 	unsigned int i = 0;
 
 	if (!h)
@@ -24,26 +43,17 @@ int delete_dnodeint_at_index(dlistint_t **h, unsigned int idx)
 		{
 			if (previous)
 			{
-
-				if (current->next)
-				{
-					previous->next = current->next;
-					current->next->prev = previous;
-				}
-				else
-				{
-					previous->next = NULL;
-				}
-				free(current);
-				return (1);
+				return (new_func(current, previous));
 			}
-			else{
+			else
+			{
 				if (current->next)
 				{
 					current->next->prev = NULL;
 					(*h) = current->next;
 				}
-				else{
+				else
+				{
 					(*h) = NULL;
 				}
 				free(current);
@@ -51,7 +61,7 @@ int delete_dnodeint_at_index(dlistint_t **h, unsigned int idx)
 			}
 		}
 		i++;
-		previous = current; 
+		previous = current;
 		current = current->next;
 	}
 	free(current);
